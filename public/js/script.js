b4e823e16ef7bc0eab18c1d75e358c43d5522aed
tiochoirul <mtio871@gmail.com>
2023-07-30 18:17:51 +0700

    change theme

--- public/js/script.js
+++ public/js/script.js
@@ -31,25 +31,3 @@ window.addEventListener('click', function(e) {
         navMenu.classList.add('hidden');
     }
 });
-
-//Dark Mode Toggle
-const darkToggle = document.querySelector('#dark-toggle');
-const html = document.querySelector('html');
-
-darkToggle.addEventListener('click', function() {
-    if (darkToggle.checked) {
-        html.classList.add('dark');
-        localStorage.theme = 'dark';
-    }
-    else {
-        html.classList.remove('dark');
-        localStorage.theme = 'light';
-    }
-});
-
-// pindahkan toggel sesuai mode
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    darkToggle.checked = true;
-} else {
-    darkToggle.checked = false;
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,11 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '#14b8a6',
-        secondary: "#64748b",
-        dark: '#0f172a'
+        primary: '#A0C1B8',
+        darkbg: '#525252',
+        lightbg: '#DDDDDD',
+        secondary: "#C0C0C0",
+        dark: '#313131'
       }
     },
   },
